4a6486c94a9462f835dcd960c07b867c702a4a77
hanamartinkovakuchynkova <h.m.kuchynkova@gmail.com>
2024-06-10 10:30:52 +0200

    Business card display app

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/entity/Vizitka.java
@@ -0,0 +1,117 @@
+package cz.czechitas.java2webapps.ukol6.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+import org.springframework.data.annotation.Id;
+
+@Entity
+@Table(name = "Vizitka")
+public class Vizitka {
+
+    @jakarta.persistence.Id
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String celeJmeno;
+    private String firma;
+    private String ulice;
+    private String obec;
+    private String psc;
+    private String email;
+    private String telefon;
+    private String web;
+
+    public Vizitka() {
+    }
+
+    public Vizitka(Integer id, String celeJmeno, String firma, String ulice, String obec, String psc, String email, String telefon, String web) {
+        this.id = id;
+        this.celeJmeno = celeJmeno;
+        this.firma = firma;
+        this.ulice = ulice;
+        this.obec = obec;
+        this.psc = psc;
+        this.email = email;
+        this.telefon = telefon;
+        this.web = web;
+    }
+
+    public String celaAdresa() {
+        return ulice + ", " + psc + obec;
+    }
+
+    public String getCeleJmeno() {
+        return celeJmeno;
+    }
+
+    public void setCeleJmeno(String celeJmeno) {
+        this.celeJmeno = celeJmeno;
+    }
+
+    public String getFirma() {
+        return firma;
+    }
+
+    public void setFirma(String firma) {
+        this.firma = firma;
+    }
+
+    public String getUlice() {
+        return ulice;
+    }
+
+    public void setUlice(String ulice) {
+        this.ulice = ulice;
+    }
+
+    public String getObec() {
+        return obec;
+    }
+
+    public void setObec(String obec) {
+        this.obec = obec;
+    }
+
+    public String getPsc() {
+        return psc;
+    }
+
+    public void setPsc(String psc) {
+        this.psc = psc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefon() {
+        return telefon;
+    }
+
+    public void setTelefon(String telefon) {
+        this.telefon = telefon;
+    }
+
+    public String getWeb() {
+        return web;
+    }
+
+    public void setWeb(String web) {
+        this.web = web;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+}
